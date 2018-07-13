e814d8ad18592e9ea6a48a75e10d62be606dc1b0
Howard Feger <hfeger4@gmail.com>
2018-07-13 13:43:48 -0700

    add edit update and partial

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  # before_action :set_user, only: [:edit, :update, :show, :destroy]
   def new
     @user = User.new
   end
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Wecome to the Alpha Blog #{@user.username}"
+      flash[:success] = "Wecome to the Alpha Blog, #{@user.username}"
       redirect_to articles_path
     else
       render 'new'
@@ -15,9 +15,17 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:success] = "You have updated your profile"
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
   end
 
   def show
@@ -28,10 +36,10 @@ class UsersController < ApplicationController
 
   private
   def user_params
-    params.require(:user).permit(:username,:password,:email)
+    params.require(:user).permit(:username,:email,:password,)
   end
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 end
