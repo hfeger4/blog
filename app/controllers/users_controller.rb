e7f8a065e7362ab748c9a2cf267756f2d285a1db
Howard Feger <hfeger4@gmail.com>
2018-07-12 20:03:02 -0700

    add framework for user show, edit, update, and destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
   def new
     @user = User.new
   end
@@ -13,8 +14,24 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+
+  def destroy
+  end
+
   private
   def user_params
     params.require(:user).permit(:username,:password,:email)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
